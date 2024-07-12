8fc981b4fb663af1ea6608be9574a2f6f5c86b20
sabri taieb <sabritaiebdev@gmail.com>
2024-07-12 17:09:38 +0100

    Add assertUrl helper

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -6,5 +6,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function assertUrl(string $str): void
+    {
+        $this->assertTrue(filter_var($str, FILTER_VALIDATE_URL) !== false, sprintf('Failed asserting that %s is an URL.', $str));
+    }
 }
