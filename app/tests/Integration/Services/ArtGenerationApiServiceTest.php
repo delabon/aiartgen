844b0dd168e56ab86282cd945d7b34894a0116e2
sabri taieb <sabritaiebdev@gmail.com>
2024-07-12 18:31:23 +0100

    Add integration tests for ArtGenerationApiService

---
+++ app/tests/Integration/Services/ArtGenerationApiServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Integration\Services;
+
+use App\Exceptions\InvalidApiKeyException;
+use App\Services\ArtGenerationApiService;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\TestCase;
+
+class ArtGenerationApiServiceTest extends TestCase
+{
+    private string $artGeneratedPath = '';
+
+    protected function tearDown(): void
+    {
+        @unlink($this->artGeneratedPath);
+
+        parent::tearDown();
+    }
+
+    public function test_requests_an_art_successfully(): void
+    {
+        $artGenerationApiService = new ArtGenerationApiService(
+            env('APP_OPENAI_API_KEY'),
+            new Client()
+        );
+
+        $result = $artGenerationApiService->request('Dancing dogs.', 1024, 1024);
+
+        $this->assertIsString($result);
+        $this->assertUrl($result);
+    }
+
+    #[DataProvider('invalidApiKeyDataProvider')]
+    public function test_throws_invalid_api_key_exception_when_invalid_api_key(string $apiKey): void
+    {
+        $artGenerationApiService = new ArtGenerationApiService(
+            $apiKey,
+            new Client()
+        );
+
+        $this->expectException(InvalidApiKeyException::class);
+        $this->expectExceptionMessage('Invalid API Key.');
+
+        $artGenerationApiService->request('Dancing dogs.', 1024, 1024);
+    }
+
+    public static function invalidApiKeyDataProvider (): array
+    {
+        return [
+            'Empty api key' => [
+                'apiKey' => ''
+            ],
+            'Invalid api key' => [
+                'apiKey' => 'invalid apu key'
+            ],
+        ];
+    }
+}
