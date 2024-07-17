320fa69231379b556c326623285cc07ee3719bd1
sabri taieb <sabritaiebdev@gmail.com>
2024-07-17 09:50:28 +0100

    add new color to Tailwind CSS

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'dribbble' : '#F8F7F4'
+            }
+        },
     },
     plugins: [],
 }
