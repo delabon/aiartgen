b9323bce80e0e105bfaff4c652203860c81dbba1
sabri taieb <sabritaiebdev@gmail.com>
2024-07-17 10:21:13 +0100

    Update arts.index view

--- app/resources/views/arts/index.blade.php
+++ app/resources/views/arts/index.blade.php
@@ -1,15 +1,17 @@
-<h1>Explore inspiring designs</h1>
+<x-layout>
+    <h1 class="font-bold text-4xl">Explore inspiring designs</h1>
 
-<ul>
-    @foreach ($arts as $art)
+    <ul>
+        @foreach ($arts as $art)
         <li>
             <a href="/arts/{{ $art->id }}">
                 <img src="{{ url('image/') . '/' . $art->id }}">
             </a>
         </li>
-    @endforeach
-</ul>
+        @endforeach
+    </ul>
 
-<div>
-    {{ $arts->links() }}
-</div>
+    <div>
+        {{ $arts->links() }}
+    </div>
+</x-layout>
