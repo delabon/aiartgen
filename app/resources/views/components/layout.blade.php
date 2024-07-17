74ddf78d856b063b5f9cc0b791caf702345d53cc
sabri taieb <sabritaiebdev@gmail.com>
2024-07-17 10:20:49 +0100

    Add layout.blade.php

---
+++ app/resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="mx-auto max-w-7xl">
+        {{ $slot }}
+    </div>
+</body>
+</html>
