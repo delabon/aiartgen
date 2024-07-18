b13913ea0630c040df81530c834dfb3f07eff9f6
sabri taieb <sabritaiebdev@gmail.com>
2024-07-18 07:35:57 +0100

    Group the /arts routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -8,11 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/arts')->group(function () {
-    Route::get('/', [ArtController::class, 'index']);
-    Route::post('/', [ArtController::class, 'store'])->middleware('auth');
-    Route::get('/create', [ArtController::class, 'create'])->middleware('auth');
-    Route::get('/{art}', [ArtController::class, 'show']);
+Route::prefix('/arts')->controller(ArtController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store')->middleware('auth');
+    Route::get('/create', 'create')->middleware('auth');
+    Route::get('/{art}', 'show');
 });
 
 Route::get('/image/{art}', [ImageController::class, 'show']);
