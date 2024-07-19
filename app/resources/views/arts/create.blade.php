89230e0e255b59017585c3259ebeece297ff0528
sabri taieb <sabritaiebdev@gmail.com>
2024-07-19 09:57:08 +0100

    Display old art values on art creation fails

--- app/resources/views/arts/create.blade.php
+++ app/resources/views/arts/create.blade.php
@@ -6,13 +6,13 @@
 
         <div class="mb-5">
             <x-form-label for="prompt">What's on your mind?</x-form-label>
-            <x-form-input type="text" name="prompt" id="prompt" placeholder="Make hero dogs" :required="true"/>
+            <x-form-input type="text" name="prompt" id="prompt" placeholder="Make hero dogs" :required="true" :value="old('prompt')"/>
             <x-form-error name="prompt"/>
         </div>
 
         <div class="mb-5">
             <x-form-label for="title">Give your art a title</x-form-label>
-            <x-form-input type="text" name="title" id="title" placeholder="Dogs are our heroes" :required="true"/>
+            <x-form-input type="text" name="title" id="title" placeholder="Dogs are our heroes" :required="true" :value="old('title')"/>
             <x-form-error name="title"/>
         </div>
 
