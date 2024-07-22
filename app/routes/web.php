cbabdf8e6c9eddb5fdceb2d7cc8f73cec457cd47
sabri taieb <sabritaiebdev@gmail.com>
2024-07-22 09:42:31 +0100

    Tiny improvements to routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -10,12 +10,12 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::prefix('/arts')->controller(ArtController::class)->group(function () {
-    Route::get('/', 'index')->name('arts.index');
+Route::prefix('/arts')->name('arts.')->controller(ArtController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
     Route::post('/', 'store')->middleware('auth');
     Route::get('/create', 'create')->middleware('auth');
-    Route::get('/@/{user:username}', 'userArt')->name('arts.user.art');
-    Route::get('/{art}', 'show')->name('arts.show');
+    Route::get('/@/{user:username}', 'userArt')->name('user.art');
+    Route::get('/{art}', 'show')->name('show');
 });
 
 Route::get('/image/{art}', [ImageController::class, 'show'])->name('image.show');
@@ -27,7 +27,7 @@ Route::prefix('/login')->controller(LoginController::class)->group(function () {
 
 Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::prefix('/register')->controller(RegisterController::class)->group(function () {
-    Route::get('/', 'create')->name('register.create');
-    Route::post('/', 'store')->name('register.store');
+Route::prefix('/register')->name('register.')->controller(RegisterController::class)->group(function () {
+    Route::get('/', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
 });
