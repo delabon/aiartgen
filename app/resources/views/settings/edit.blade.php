f6119f5567427e2e7e41fd8c64cef946ba6f8ae5
sabri taieb <sabritaiebdev@gmail.com>
2024-08-14 10:48:26 +0100

    Add delete account section to the settings page

--- app/resources/views/settings/edit.blade.php
+++ app/resources/views/settings/edit.blade.php
@@ -52,4 +52,13 @@
 
         <x-form-button>Update</x-form-button>
     </form>
+
+    <hr class="my-6">
+
+    <form class="w-full mx-auto" action="{{ route('settings.destroy') }}" method="post">
+        @csrf
+        @method('delete')
+
+        <x-form-button class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete account</x-form-button>
+    </form>
 </x-layout>
